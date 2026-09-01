78e2f1f7789881726248c5a0edfb6d5911a3738a
Alexander Yoshida <alexander.yoshida@gmail.com>
2026-09-01 00:42:11 -0400

    add problem from scrim

---
+++ misc/coffeecentral.cpp
@@ -0,0 +1,212 @@
+#include <cmath>
+#include <limits>
+#include <random>
+#include <queue>
+#include <array>
+#include <thread>
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+
+using namespace std;
+using namespace std::chrono_literals;
+
+// using namespace __gnu_pbds;
+// template <class T>
+// using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <class T>
+// using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define MOD 1000000009
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+const ld ROT_HALF = 3.14159265358979323846L;
+const ld ROT_FULL = ROT_HALF * 2.L;
+const ld ROT_QUARTER = ROT_HALF * 0.5L;
+
+ostream& operator<<(ostream& os, i128 x) {
+    if (x < 0) {
+        x = -x;
+        os << '-';
+    }
+    static char s[40];
+    int i = 0;
+    while (x) {
+        s[i++] = '0' + x % 10;
+        x /= 10;
+    }
+    if (i == 0) s[i++] = '0';
+    while (i) os << s[--i];
+    return os;
+}
+
+istream& operator>>(istream& is, i128 &x) {
+    static string s; is >> s;
+    x = 0;
+    bool neg = s[0] == '-';
+    for (int i = 0; i < s.size(); ++i) {
+        x = x * 10 + (s[i] - '0');
+    }
+    if (neg) x = -x;
+    return is;
+}
+
+
+ostream& operator<<(ostream& os, u128 x) {
+    static char s[40];
+    int i = 0;
+    while (x) {
+        s[i++] = '0' + x % 10;
+        x /= 10;
+    }
+    if (i == 0) s[i++] = '0';
+    while (i) os << s[--i];
+    return os;
+}
+
+istream& operator>>(istream& is, u128 &x) {
+    static string s; is >> s;
+    x = 0;
+    bool neg = s[0] == '-';
+    for (int i = 0; i < s.size(); ++i) {
+        x = x * 10 + (s[i] - '0');
+    }
+    if (neg) x = -x;
+    return is;
+}
+
+template <typename T,typename U>                                                   
+pair<T,U> operator+(const pair<T,U> & l,const pair<T,U> & r) {   
+    return {l.first+r.first,l.second+r.second};                                    
+}
+template <typename T,typename U>         
+pair<T,U> operator-(const pair<T,U> & l,const pair<T,U> & r) {   
+    return {l.first-r.first,l.second-r.second};                                    
+}
+
+template <typename... Args>
+ostream& operator<<(ostream& os, const tuple<Args...>& t) {
+    os << "(";
+    apply([&os](const auto&... args) {
+        size_t n = 0;
+        ((os << (n++ ? ", " : "") << args), ...);
+    }, t);
+    return os << ")";
+}
+
+template <typename... Args>
+istream& operator>>(istream& is, tuple<Args...>& t) {
+    apply([&is](auto&... args) {
+        ((is >> args), ...);
+    }, t);
+    return is;
+}
+
+template <typename T, size_t N>
+ostream& operator<<(ostream& os, const array<T, N>& a) {
+    os << "[";
+    for (size_t i = 0; i < N; ++i) { 
+        os << a[i]; 
+        if (i != N - 1) 
+            os << ", "; 
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T, size_t N>
+istream& operator>>(istream& is, array<T, N> &a) {
+    for (auto &x : a) {
+        is >> x;
+    }
+    return is;
+}
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) {
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i) { 
+        os << v[i]; 
+        if (i != v.size() - 1) 
+            os << ", "; 
+    }
+    os << "]";
+    return os;
+}
+template<typename A, typename B> istream& operator>>(istream &is, pair<A, B> &p) { return is >> p.first >> p.second; }
+template<typename T> istream& operator>>(istream &is, vector<T> &v) {
+    for (auto &x : v) {
+        is >> x;
+    }
+    return is;
+}
+
+const ll INF = LLONG_MAX >> 2;
+
+#define EPS 1e-12
+#define each(i, x) for (auto &i : x)
+#define mp(a,b) make_pair(a,b)
+#define pause() this_thread::sleep_for(10ms)
+
+bool solve(int tc) {
+    int rows, cols, n, q; cin >> cols >> rows >> n >> q;
+    if (rows == 0) return false;
+
+    cout << "Case " << tc << ":\n";
+    vector<vector<int>> psum(rows + cols + 1, vector<int>(rows + cols + 1, 0));
+    while (n--) {
+        int r, c; cin >> c >> r; --r; --c;
+        int u = r + c, v = cols + r - c - 1;
+        psum[u+1][v+1] += 1;
+    }
+    int cap = rows + cols;
+    for (int r = 1; r <= cap; ++r) {
+        for (int c = 1; c <= cap; ++c) {
+            psum[r][c] += psum[r-1][c] + psum[r][c-1] - psum[r-1][c-1];
+        }
+    }
+    while (q--) {
+        int s; cin >> s;
+        int best = -1;
+        int br = 0;
+        int bc = 0;
+        for (int r = 0; r < rows; ++r) {
+            int u = r;
+            int v = r+cols-1;
+            int us = max(r-s, 0);
+            int ue = min(r+s+1, cap);
+            int vs = max(v-s, 0);
+            int ve = min(v+s+1, cap);
+            for (int c = 0; c < cols; ++c, ++u, --v) {
+                int t = psum[ue][ve] - psum[us][ve] - psum[ue][vs] + psum[us][vs];
+                us += u-s >= 0;
+                ue += ue < cap;
+                vs -= vs > 0;
+                ve -= v+s+1 <= cap;
+                if (t <= best) continue;
+
+                best = t;
+                br = r;
+                bc = c;
+            }
+        }
+        cout << best << " (" << bc+1 << "," << br+1 << ")\n";
+    }
+    return true;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cout << fixed << setprecision(9);
+    int tc = 0;
+    while (solve(++tc));
+}
